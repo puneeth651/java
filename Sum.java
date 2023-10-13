dee4c71177eed6a90de48fb168b2184fdee34110
puneeth <dpoojithareddy23@gmail.com>
2023-10-13 11:39:38 +0530

    My first commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, Sum;  
+Sum = n1 + n2;  
+System.out.println("The Sum of numbers is: "+Sum);  
+}  
+}  
